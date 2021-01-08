f0384b129c2502d53db2345165833fd585820aec
ASwartz14 <asriley144@gmail.com>
2021-01-07 21:45:56 -0800

    finally got api routes working, was missing /

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const notes = require("../db/db.json");
+const nanoID = require("nanoid").nanoid;
 
 module.exports = function (app) {
   // Displays all notes
@@ -7,9 +8,28 @@ module.exports = function (app) {
     return res.json(notes);
   });
 
-  app.post("api/notes", function (req, res) {
-    const newNote = req.body;
+  app.post("/api/notes", function (req, res) {
+    const notesdb = notes;
+    const newNote = {
+      id: nanoID(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    notesdb.push(newNote);
+    fs.writeFile("app/db/db.json", JSON.stringify(notesdb), function () {
+      res.json(notesdb);
+      console.log("posted");
+      console.log(notesdb);
+    });
   });
 
-  app.delete("api/notes/:id", function (req, res) {});
+  app.delete("/api/notes/:id", function (req, res) {
+    const notesdb = notes;
+    const deleteNote = notesdb.filter((note) => note.id === req.params.id);
+    notesdb.splice(deleteNote, 1);
+    fs.writeFile("app/db/db.json", JSON.stringify(notesdb), function () {
+      res.json(notesdb);
+      console.log("deleted");
+    });
+  });
 };
